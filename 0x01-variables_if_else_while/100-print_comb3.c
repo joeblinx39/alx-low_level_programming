e10fc2056cee44aa89e2abd557c78f7eee7022e7
joeblinx39 <joeblinx39@gmail.com>
2023-02-19 16:39:36 -0800

    Print all possible different combinations of two digits

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Prints all possible combinations of two digits
+ * separated by ',' and '' using putchar function
+ *
+ *
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int num_1, num_2;
+
+	for (num_1 = 0; num_1 <= 8; num_1++)
+	{
+		for (num_2 = num_1; num_2 <= 9; num_2++)
+		{
+			putchar(num_1 % 10 + '0');
+			putchar(num_2 % 10 + '0');
+
+			if (num_1 < 8)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+
+	putchar('\n');
+
+	return (0);
+}
