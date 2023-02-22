171b8ce682a783bd3d2fa0e4a2569132e30af830
joeblinx39 <joeblinx39@gmail.com>
2023-02-21 18:35:53 -0800

    Complete task nine

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,38 @@
+#include "main.h"
+
+/**
+ * times_table - Prints the 9 times table
+ */
+void times_table(void)
+{
+	int row, col, product;
+
+	for (row = 0; row <= 9; row++)
+	{
+		for (col = 0; col <= 9; col++)
+		{
+			product = row * col;
+			if (col == 0)
+			{
+				_putchar('0');
+			}
+			else if (product <= 9)
+			{
+				_putchar(' ');
+				_putchar(product + '0');
+			}
+			else
+			{
+				_putchar((product / 10) + '0');
+				_putchar((product % 10) + '0');
+			}
+
+			if (col < 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+		}
+		_putchar('\n');
+	}
+}
