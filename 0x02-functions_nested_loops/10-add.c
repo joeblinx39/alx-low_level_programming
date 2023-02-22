fef62ddf9132ebc9d9cb3fec4cdb48673ee41065
joeblinx39 <joeblinx39@gmail.com>
2023-02-21 19:40:33 -0800

    Complete task ten

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * add - prints the result of the addition of two ints
+ * @num_1: integer 1
+ * @num_2: integer 2
+ * Return: num_1 + num_2
+ */
+int add(int num_1, int num_2)
+{
+	int sum;
+
+	sum = num_1 + num_2;
+
+	return (sum);
+}
