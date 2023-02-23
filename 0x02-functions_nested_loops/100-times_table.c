ee100b2b3405e0d12573f8a32bee005c5c480598
joeblinx39 <joeblinx39@gmail.com>
2023-02-22 17:13:01 -0800

    Complete task twelve

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,47 +1,47 @@
 #include "main.h"
 
 /**
- * print_times_table - prints the n times table, starting with 0
- * @n: number of times table to print
- *
- * Return: void
+ * print_times_table - prints the times table of any int
+ * @n: int value for the table
  */
 void print_times_table(int n)
 {
-	int i, j, result;
+	int num1, num2, num3;
 
-	if (n < 0 || n > 15)
-		return;
-
-	for (i = 0; i <= n; i++)
+	if (n >= 0 && n <= 15)
 	{
-		for (j = 0; j <= n; j++)
+		for (num1 = 0; num1 <= n; num1++)
 		{
-			result = i * j;
-			if (j == 0)
-				_putchar('0');
-			else
+			for (num2 = 0; num2 <= n; num2++)
 			{
-				_putchar(',');
-				_putchar(' ');
-				_putchar(' ');
-				_putchar(' ');
-				_putchar(' ');
-				_putchar(' ');
-				_putchar(' ');
-				_putchar(' ');
-				_putchar(' ');
-				if (result < 100)
+				num3 = num2 * num1;
+				if (num2 == 0)
+				{
+					_putchar(num3 + '0');
+				} else if (num3 < 10 && num2 != 0)
+				{
+					_putchar(',');
 					_putchar(' ');
-				if (result < 10)
 					_putchar(' ');
-				if (result >= 100)
-					_putchar((result / 100) + '0');
-				if (result >= 10)
-					_putchar(((result / 10) % 10) + '0');
-				_putchar((result % 10) + '0');
+					_putchar(' ');
+					_putchar(num3 + '0');
+				} else if (num3 >= 10 && num3 < 100)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar((num3 / 10) + '0');
+					_putchar((num3 % 10) + '0');
+				} else if (num3 >= 100)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar((num3 / 100) + '0');
+					_putchar(((num3 / 10) % 10) + '0');
+					_putchar((num3 % 10) + '0');
+				}
 			}
+			_putchar('\n');
 		}
-		_putchar('\n');
 	}
 }
