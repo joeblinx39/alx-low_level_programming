8725a7a3f2dc4325e88a0371e18dece76f4be358
joeblinx39 <joeblinx39@gmail.com>
2023-02-23 03:25:01 -0800

    Complete task one which print the multipication of two integer

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * mul - prints the result of the multiplication of two integars
+ * @a: integer 1
+ * @b: integer 2
+ * Return: num_1 + num_2
+ */
+int mul(int a, int b)
+{
+	int mul;
+
+	mul = a * b;
+
+	return (mul);
+}
