13ccdaa119d611a27013c53fa94c6c33fbcd1c23
joeblinx39 <joeblinx39@gmail.com>
2023-02-22 20:16:43 -0800

    Complete task sixteen

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+
+/**
+ * print_fibonacci - prints the first 98 Fibonacci numbers
+ *
+ * Return: void
+ */
+void print_fibonacci(void)
+{
+	unsigned long fib1 = 1, fib2 = 2, fib_sum;
+	int i;
+
+	printf("%lu, %lu", fib1, fib2);
+	for (i = 3; i <= 98; i++)
+	{
+		fib_sum = fib1 + fib2;
+		printf(", %lu", fib_sum);
+		fib1 = fib2;
+		fib2 = fib_sum;
+	}
+	printf("\n");
+}
+
