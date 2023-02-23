991fb19bdba0402704e3551fa05d5e621077c59f
joeblinx39 <joeblinx39@gmail.com>
2023-02-23 11:31:40 -0800

    Complete task one which print a range of integer excluding 2 and 4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints integers from 0 - 9, then a new line
+ */
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		if (c != '2' && c != '4')
+		{
+			_putchar(i + '0');
+		}
+	}
+	_putchar('\n');
+}
