1f7a887cb46e20202625502d05bb92b7bdf27e7a
joeblinx39 <joeblinx39@gmail.com>
2023-02-23 16:21:41 -0800

    Complete task eight

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_line - Print lines if value is above zero
+ *
+ * @size: number of times the _ character is to be printed
+ */
+void print_square(int size)
+{
+	int width, height;
+
+	if (size > 0)
+	{
+		for (width = 0; width < size; width++)
+		{
+			for (height = 0; height < size; height++)
+				_putchar('#');
+			_putchar('\n');
+		}
+	}
+	else
+		_putchar('\n');
+}
