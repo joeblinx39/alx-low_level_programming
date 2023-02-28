b5f828a6ef670ad3960a5b301beeb33a8aa61d9d
joeblinx39 <joeblinx39@gmail.com>
2023-02-27 18:40:23 -0800

    Print n elements of an array of integers, followed by a new line

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_array - prints n elements of an array of integers,
+ *               followed by a new line.
+ * @a: Pointer to the first element of the array.
+ * @n: Number of elements of the array to be printed.
+ *
+ * Return: void.
+ */
+void print_array(int *a, int n)
+{
+	int index;
+
+	for (index = 0; index < n; index++)
+	{
+		printf("%d", a[index]);
+
+		if (index != n - 1)
+			printf(", ");
+	}
+
+	printf("\n");
+}
