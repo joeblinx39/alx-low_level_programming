4932d8b69956d94d7dc36a8df51db63fe2991b0f
joeblinx39 <joeblinx39@gmail.com>
2023-02-28 14:34:25 -0800

    Convert a string to an integer, followed by a new line

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -9,20 +9,18 @@
  */
 int _atoi(char *s)
 {
-	int sign = 1, num = 0;
+	int sign = 1;
 
-	while (*s)
-	{
+	unsigned int num = 0;
+
+	do {
 		if (*s == '-')
 			sign *= -1;
-		if (*s >= '0' && *s <= '9')
-		{
-			num = num * 10 + (*s - '0');
-			if (*(s + 1) < '0' || *(s + 1) > '9')
-				break;
-		}
-		s++;
-	}
+		else if (*s >= '0' && *s <= '9')
+			num = (num * 10) + (*s - '0');
+		else if (num > 0)
+			break;
+	} while (*s++);
 
-	return num * sign;
+	return (num * sign);
 }
