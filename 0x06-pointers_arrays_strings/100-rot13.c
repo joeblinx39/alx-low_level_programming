d65aacbbd2824cfae3467902da8f5eff84906347
joeblinx39 <joeblinx39@gmail.com>
2023-03-02 17:58:44 -0800

    encode a string using rot13

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * leet - encodes a string into 1337.
+ * @str: the input string to be encoded
+ * Return: pointer to str
+ */
+char *rot13(char *str)
+{
+	int index1, index2;
+	char letters[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char rots[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
+	for (index1 = 0; str[index1] != '\0'; index1++)
+	{
+		for (index2 = 0; letters[index2] != '\0'; index2++)
+		{
+			if (str[index1] == letters[index2])
+			{
+				str[index1] = rots[index2];
+				break;
+			}
+		}
+	}
+
+	return (str);
+}
+
