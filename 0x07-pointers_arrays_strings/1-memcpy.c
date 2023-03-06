00198e36b3be895fafd39864d870194f0f56b904
joeblinx39 <joeblinx39@gmail.com>
2023-03-06 13:00:17 -0800

    Copy memory area

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -10,11 +10,9 @@
  */
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-	char *d = dest;
+	unsigned int i;
 
-	char *s = src;
-
-	for (unsigned int i = 0; i < n; i++)
+	for (i = 0; i < n; i++)
 	{
 		d[i] = s[i];
 	}
