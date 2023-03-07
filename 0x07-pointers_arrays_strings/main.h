c72664747a58a13ea3a1a3d7aaf97a889516c0ad
joeblinx39 <joeblinx39@gmail.com>
2023-03-06 20:00:36 -0800

    Fix and update the main header file

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -4,9 +4,11 @@
 /* Header files */
 #include <stdio.h>
 #include <stdlib.h>
+#include <stddef.h>
 
 /* Function prototypes */
-char *_memset(char *s, char b, unsigned int n);
+int _putchar(char c);
+void *_memset(void *s, int c, size_t n);
 char *_memcpy(char *dest, char *src, unsigned int n);
 char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
