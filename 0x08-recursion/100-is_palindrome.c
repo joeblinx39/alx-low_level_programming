26ffa7e15f0c5e97baaa6c416a8872a9b7480cc9
joeblinx39 <joeblinx39@gmail.com>
2023-03-09 19:33:56 -0800

    returns 1 if a string is a palindrome and 0 if not

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,23 +1,16 @@
 #include "main.h"
 #include <stdio.h>
 
-int check_palindrome(char *s, int i, int j);
-
 /**
- * is_palindrome - checks if a string is a palindrome
- *
- * @s: the string to check
- *
- * Return: 1 if s is a palindrome, 0 otherwise
+ * _strlen_recursion - Returns the length of a string recursively
+ * @s: Pointer to the string to be measured
+ * Return: The length of the string
  */
-int is_palindrome(char *s)
+int _strlen_recursion(char *s)
 {
-	int len = 0;
-
-	while (s[len])
-		len++;
-
-	return (check_palindrome(s, 0, len - 1));
+	if (s[0] != '\0')
+		return (1 + _strlen_recursion(s + 1));
+	return (0);
 }
 
 /**
@@ -32,11 +25,23 @@ int is_palindrome(char *s)
  */
 int check_palindrome(char *s, int i, int j)
 {
-	if (i >= j)
+	if (s[i] == s[j])
+	{
+		if (i > j / 2)
 		return (1);
-
-	if (s[i] != s[j])
+	}
+	else
+		return (check_palindrome(s, i + 1, j - 1));
+	else
 		return (0);
-
-	return (check_palindrome(s, i + 1, j - 1));
 }
+
+/**
+ * is_palindrome - Check if a string is a palindrome
+ * @s: Pointer to the base address of the string
+ * Return: 1 if the string is a palindrome, 0 otherwise
+ */
+int is_palindrome(char *s)
+{
+	return (check_palindrome(s, 0, _strlen_recursion(s) - 1));
+
