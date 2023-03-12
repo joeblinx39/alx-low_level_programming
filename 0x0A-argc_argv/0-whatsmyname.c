869b509379b66e86714a660c68873440c36a718c
joeblinx39 <joeblinx39@gmail.com>
2023-03-11 17:27:25 -0800

    Fix bug: add (void)argc to suppress unused arguments

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,16 +1,20 @@
-#include <stdio>
+#include <stdio.h>
+#include <stdlib.h>
 
 /**
- * main - Prints the name of a program
- *
- * @argc: number of command-line argument
- *
- * @argv: string of argument passed on main func
- *
- * Return: Always (0) (Success)
+ * main - Prints the name of the program
+ * followed by a new line
+ * @argc : number of command-line arguments
+ * @argv : string of argumenst passed to main fcn
+ * Return: Always 0 (Success)
  */
+
 int main(int argc, char *argv[])
 {
+/* cast argc to void to suppress unused variable warning */
+	(void)argc;
+
+
 	printf("%s\n", argv[0]);
 
 	return (0);
